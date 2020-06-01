71e7e0d2426be44cd1a313c0ea102a22fee6a61a
IsinghGitHub <indrajitsingh37@gmail.com>
2020-06-02 00:42:43 +0530

    test changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,13 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+        <h1>My Story</h1>
+        <p>Hello Everyone! My name is Indrajit Singh and I am a Senior Software Engineer and AI Researcher. I love to explore new technologies, learn things and help others learn.</p>
+        <p>"I have complete faith in the focus and the energy that drives me to do greater things in life and that's my intention and love towards executing any task. Instead of just applying for a job randomly I keep my faith in creating interest and attention towards the job"</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+   
   </Layout>
 )
 
